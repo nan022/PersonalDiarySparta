7295c211061b6576c34c8634291ce0093313b540
Ronanda <ronanda20ti@mahasiswa.pcr.ac.id>
2023-02-03 17:16:29 +0700

    Second Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+today = datetime.now()
+date_time = today.strftime('%Y-%m-%d %H:%M:%S')
+print(date_time)
+
+
+
+
+
+# first_name = 'Joseph'
+# last_name = 'Choi'
+# full_name = f'{first_name} {last_name}'
+# print(full_name)
